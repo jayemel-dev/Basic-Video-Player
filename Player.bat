41960824bbb59ad2d77df86908023067d72ccb1f
Jaiden Little <jaiden213little8@gmail.com>
2026-09-23 18:50:50 +1000

    changed bat file to link ico to exe

--- Player.bat
+++ Player.bat
@@ -1,6 +1,6 @@
 @echo off
 
-g++ -std=c++17 source/private/*.cpp -o Player.exe -I"C:/msys64/mingw64/include/opencv5" -L"../lib" -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_videoio -lSDL2main -lSDL2 -lsfml-audio -lsfml-system -fdiagnostics-color=never > log.txt 2>&1
+g++ -std=c++17 source/private/*.cpp app.res -o Player.exe -I"C:/msys64/mingw64/include/opencv5" -L"../lib" -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lopencv_videoio -lSDL2main -lSDL2 -lsfml-audio -lsfml-system -fdiagnostics-color=never > log.txt 2>&1
 
 if %errorlevel% neq 0 (
     echo Compilation failed. Check log.txt
